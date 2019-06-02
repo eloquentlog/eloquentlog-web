08d9f0e61255ebacb4bb3d98954c032210e8d0c5
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2019-06-03 03:11:20 +0900

    Update login form

--- src/login.ts
+++ src/login.ts
@@ -1,5 +1,32 @@
+import { linkEvent } from 'inferno';
 import { h } from 'inferno-hyperscript';
 
-export const Login = () => {
-  return h('.content', h('h2', 'Login'));
+const handleSubmit = (props: any, event: Event) => {
+  event.preventDefault();
+  console.log(props);
+};
+
+export const Login = (props: any) => {
+  return h('.content', [
+    h('form', {
+      noValidate: true
+    , onSubmit: linkEvent(props, handleSubmit)
+    }, [
+      h('.control-group', [
+        h('label.label', { for: 'username' }, 'Username')
+      , h('input#username', { type: 'email', name: 'username' })
+      ])
+    , h('.control-group', [
+        h('label.label', { for: 'password' }, 'Password')
+      , h('input#password', { type: 'password', name: 'password' })
+      ])
+    , h('button#submit', { type: 'submit' }, 'Login')
+    ])
+  ]);
+};
+
+Login.defaultHooks = {
+  onComponentDidMount (_: any) {
+    document.title = 'Login - ' + document.title;
+  }
 };
