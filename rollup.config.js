b5a6239a0e3ab46b32df98bdbafe0b665f4394c9
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-09-03 16:56:29 +0900

    Update dev server host settings

--- rollup.config.js
+++ rollup.config.js
@@ -154,7 +154,7 @@ export default (args) => {
         , historyApiFallback: [
             { from: /.*/, to: 'index.html' }
           ]
-        , host: 'localhost'
+        , host: '127.0.0.1'
         , port: 3000
         })
       );
